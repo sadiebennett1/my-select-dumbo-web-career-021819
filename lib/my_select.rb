4ec07b809959322e9c6cda6fb9dcce06d524215b
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-12 14:47:34 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  new_arr = []
+  while i < collection.length 
+    var = yield collection[i]
+    if var == true
+      new_arr << var
+    i += 1
+  end
+  new_arr
+  
 end
+
+
+# (my_select([1,2,3,4]) do |num|
+#   num.even?
+# end 
+# 
+# to eq([2, 4])
